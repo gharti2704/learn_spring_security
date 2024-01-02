6cad6abe8f4e4b0a14f4315a9c39caca99c51f70
Raju Gharti <gharti.rg@gmail.com>
2024-01-02 16:20:46 -0500

    implement simple security

---
+++ src/main/java/com/learn_spring_security/controller/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.learn_spring_security.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/")
+public class HelloWorld {
+
+    @GetMapping("hello")
+    public String helloWorld() {
+        return "Hello World!";
+    }
+}
